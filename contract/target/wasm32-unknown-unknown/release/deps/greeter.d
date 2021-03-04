8ef3e7cfb2ba2e4cf004eacbb6e968fbb1067fdc
caner sevince <canersevince@gmail.com>
2021-03-04 19:08:48 +0300

    init

---
+++ contract/target/wasm32-unknown-unknown/release/deps/greeter.d
@@ -0,0 +1,10 @@
+C:\Users\cnrs\PhpstormProjects\nearfolio-repo-1\contract\target\wasm32-unknown-unknown\release\deps\greeter.wasm: src\lib.rs src\model.rs src\.\types.rs src\logger.rs
+
+C:\Users\cnrs\PhpstormProjects\nearfolio-repo-1\contract\target\wasm32-unknown-unknown\release\deps\libgreeter.rlib: src\lib.rs src\model.rs src\.\types.rs src\logger.rs
+
+C:\Users\cnrs\PhpstormProjects\nearfolio-repo-1\contract\target\wasm32-unknown-unknown\release\deps\greeter.d: src\lib.rs src\model.rs src\.\types.rs src\logger.rs
+
+src\lib.rs:
+src\model.rs:
+src\.\types.rs:
+src\logger.rs:
